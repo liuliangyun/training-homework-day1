fa9b655de5cb45667c199bdc1b60d1dc8c547d8e
Liangyun Liu <lyliu@CNlyliu.local>
2018-07-17 01:08:59 +0800

    second day's work: flaten is finished.

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  return [].concat.apply([],collection);
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  let result = [].concat.apply([], collection);
+  return result.filter(function (x, index, arr) {
+    return arr.indexOf(x) == index;
+  });
 }
 
 module.exports = double_to_one;
